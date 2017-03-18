b44e7d6f0964dd3a2b50b2b2cd40927ccfa869d5
Giacomo San <giacomo@Giacomo-2.local>
2017-03-18 17:34:56 -0500

    ozhe ziii

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,9 @@
+
+
 (function($){
     'use strict';
+
+
 
     //menu options
     var fixed_top = $(".main-menu");
@@ -158,3 +162,68 @@
     });
 	
 })(jQuery);	  
+
+
+//data
+
+function sendData(){
+
+  $('#nom, #cel, #dni, #ema, #type, #many, #_dir').css( "border-color", "" );
+
+  var nom = $('#nom').val();
+  var cel = $('#cel').val();
+  var dni = $('#dni').val();
+  var ema = $('#ema').val();
+  var typ = $('#type').val();
+  var afo = $('#many').val();
+  var dir = $('#_dir').val();
+  var lat = $('#lat').val();
+  var lng = $('#lng').val();
+
+  if(nom.length==0){ alert('Es necesario un nombre de contacto'); return; }
+  if(cel.length==0){ alert('Es necesario un número de contacto'); return; }
+  if(ema.length==0){ alert('Por favor dejanos tu e-mail'); return; }
+  if(typ==0){ alert('Por favor elije un tupo de espacio'); return; }
+  if(afo.length==0){ alert('Por favor indique una catidad de aforo');; return; }
+  if(dir.length==0){ alert('Es necesario conocer la direccion');; return; }
+
+  var obj = {
+      "name": nom,
+      "cel": cel,
+      "dni": dni,
+      "mail": ema,
+      "type": typ,
+      "aforo": afo,
+      "address": dir,
+      "lat": lat,
+      "lng": lng,
+      "state": 1
+  }
+  $.ajax( { url: "https://api.mlab.com/api/1/databases/santuario/collections/houses?apiKey=Jba31XhHbIXY1RBkADhfk_0XBuiKF9BT",
+    data: JSON.stringify( obj ),
+    type: "POST",
+    contentType: "application/json",
+    success: function (data) { if(data._id){ gracias(); }else{ alert("Sucedio algo en el proceso de registro, por favor intentalo nuevamnete, gracias."); } }
+    }
+  );
+}
+
+function gracias(){
+  $(".toHide").fadeOut(500);
+  $(".toShow").fadeIn(500);
+}
+
+$( document ).ready(function() {
+  // Handler for .ready() called.
+  $.get("https://api.mlab.com/api/1/databases/santuario/collections/houses?apiKey=Jba31XhHbIXY1RBkADhfk_0XBuiKF9BT", function( data ) {
+      $(".counter").html(data.length);
+  });
+});
+
+
+
+
+
+
+
+//------- xD
--- assets/js/map_functions.js
+++ assets/js/map_functions.js
@@ -1,36 +1,49 @@
 (function($){
     'use strict';
 	
-	//Home Page map
-	var styleArray = [
-	  {
-		"featureType": "water",
-		"elementType": "geometry.fill",
-		"stylers": [
+	$.get("https://api.mlab.com/api/1/databases/santuario/collections/houses?apiKey=Jba31XhHbIXY1RBkADhfk_0XBuiKF9BT", function( data ) {
+	 
+		//Home Page map
+		var styleArray = [
 		  {
-			"color": "#65ac4c"
+			"featureType": "water",
+			"elementType": "geometry.fill",
+			"stylers": [
+			  {
+				"color": "#65ac4c"
+			  }
+			]
 		  }
-		]
-	  }
-	];
+		];
 
-	var mapOptions = {
-	  center: new google.maps.LatLng(55.864237,-4.251806),
-	  zoom: 09,
-	  styles: styleArray,
-	  scrollwheel: false,
-	  backgroundColor: 'transparent',
-		mapTypeControl: false,          
-	  mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById("home-map"),
-	  mapOptions);        
-	var myLatlng = new google.maps.LatLng(55.864237,-4.251806);      
-	var marker = new google.maps.Marker({
-	  position: myLatlng,
-	  map: map,
-	  icon: 'images/map-icon.png'
+		var mapOptions = {
+		  center: new google.maps.LatLng(-6.016374,-75.142793),
+		  zoom: 5,
+		  styles: styleArray,
+		  scrollwheel: false,
+		  backgroundColor: 'transparent',
+			mapTypeControl: false,          
+		  mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById("home-map"),
+		  mapOptions);        
+		var myLatlng = new google.maps.LatLng(-6.016374,-75.142793);  
+
+		var icon = {
+		    url: 'images/marker.png', // url
+		    scaledSize: new google.maps.Size(50, 64), // scaled size
+		    origin: new google.maps.Point(0,0), // origin
+		    anchor: new google.maps.Point(0, 0) // anchor
+		};
+
+		for(var i=0; i < data.length; i++){
+			var marker = new google.maps.Marker({
+			  position: new google.maps.LatLng(data[i].lat,data[i].lng),
+			  map: map,
+			  icon: icon
+			});
+		}
+		
 	});
-	
 	
 })(jQuery);	  
